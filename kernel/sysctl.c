d2f9e56e32b087ab05a52e240ccb4f6d39acc67c
optimumpr <telez_one@yahoo.com>
2017-02-23 13:32:15 -0500

    Deny New USB Connection Part2

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -96,6 +96,9 @@
 #if defined(CONFIG_SYSCTL)
 
 /* External variables not in a header file. */
+#ifdef CONFIG_USB
+extern int deny_new_usb;
+#endif
 extern int max_threads;
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
@@ -1094,7 +1097,18 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two,
 	},
 #endif
+#ifdef CONFIG_USB
 	{
+		.procname	= "deny_new_usb",
+	 	.data		= &deny_new_usb,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+#endif				
+	{		
 		.procname	= "ngroups_max",
 		.data		= &ngroups_max,
 		.maxlen		= sizeof (int),
